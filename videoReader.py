35d4d2fd23774be41931b9129d20091576c29645
Ali Jafar <ajafar5@uic.edu>
2021-03-17 21:27:27 -0500

    pushing gui code

--- videoReader.py
+++ videoReader.py
@@ -1,20 +1,42 @@
 import cv2
 import numpy as np
 import os
+#import os.path
 
 class VideoRead():
-    def __init__(self, filename):
+    def __init__(self, path):
+
         try:
-            os.path.isfile(filename)# open(filename, "r")
+           os.path.isdir(path)
+        except NotADirectoryError or FileNotFoundError:
+            print("Not a directory")
+            exit(-1)
 
-        except IOError:
-            print("File Does not exist")
-            return
-        self.f = filename
-        self.cap = cv2.VideoCapture(self.f)
+        #except IOError:
+            #print("File Does not exist")
+            #return
+        self.path = path
+        filenames = os.listdir(self.path)
+        self.FRAMES =[]
+        filenames.sort()
+        for file in filenames:
+            #print(file)
+            frame = cv2.imread(self.path+file)
+            # with open(self.path+file) as f:
+            #     #frame = f.read()
+            #     frame = cv2.imread(f)
+            self.FRAMES.append(frame)
 
-        self.FRAMES = []
-        self.load_frames()
+        #print(self.FRAMES)
+        #print(self.files)
+        #self.files = [x for x in os.listdir(self.path) ]
+
+        #self.cap = cv2.VideoCapture(self.f)
+
+        #self.FRAMES = []
+        #self.load_frames()
+
+
         self.Kheight = 424
         self.Klength = 512
         # main window variables
@@ -34,17 +56,17 @@ class VideoRead():
 
 
 
-    def load_frames(self):
-
-        while (self.cap.isOpened()):
-            ret, frame = self.cap.read()
-
-            if ret == True:
-                self.FRAMES.append(frame)
-            else:
-                break
-
-        self.release()
+    # def load_frames(self):
+    #
+    #     while (self.cap.isOpened()):
+    #         ret, frame = self.cap.read()
+    #
+    #         if ret == True:
+    #             self.FRAMES.append(frame)
+    #         else:
+    #             break
+    #
+    #     self.release()
 
 
 
@@ -53,9 +75,9 @@ class VideoRead():
         return self.FRAMES
         pass
 
-    def release(self):
-        self.cap.release()
-        #cv2.destroyAllWindows()
+    # def release(self):
+    #     self.cap.release()
+    #     #cv2.destroyAllWindows()
 
 
     def update(self,image):
@@ -63,51 +85,65 @@ class VideoRead():
 
     def nothing(self,a):
         #cv2.imshow(self.MainWindow,'tb1')
+        print(a)
         return a
 
-        print(a)
-        pass
+        #print(a)
+
+    def show_frame(self,frame):
+        cv2.imshow("window",frame)
+        cv2.destroyWindow("window")
+        #cv2.destroyAllWindows()
+
 
     def loop(self,frames =None,delay = 1):
         if frames == None:
             frames = self.FRAMES
+       # print(frames)
 
         #trackbar1 = np.zeros((20,1080,3),np.uint8)
         #cv2.namedWindow("tb1")
-        cv2.createTrackbar("Frame #",self.MainWindow, 0, len(frames), self.nothing)
+
 
         #indexing variables for frame
-        x1, y1, x2, y2 = 4, self.Kheight + 4, 4, self.Klength + 4
+
+        frame_offsetl = (self.Wlength - 2*self.Klength) //2\
+            #, (self.Wheight-  2*self.Kheight)//2
+        #print(frame_offsetl)
+        rowmin,rowmax,colmin,colmax = 4, self.Kheight + 4, 4, self.Klength + 4
+
+        #x1, y1, x2, y2 = 4, self.Kheight + 4, 4, self.Klength + 4
         running = True
         while running:
 
-            #cv2.imshow(self.MainWindow,self.MainIMG )
+            cv2.imshow(self.MainWindow,self.MainIMG )
 
 
             # play Frames in window
             frameCounter = 0
-            slide_frame =0
-            edge_off = 20
             for f in frames:
-                frametxt = "frame #:" + str(frameCounter)
+                frametxt = "frame #:" + str(frameCounter) + "HERE IS MORE TEXT WE CAN ADD"
 
                 # background rectangle to help align video feed
                 cv2.rectangle(self.MainIMG, (2,2), (self.Klength+4,self.Kheight+4), (255, 0, 0),-1)
-                cv2.rectangle(self.MainIMG, ( self.Klength + 8,4),((self.Klength+8)*2,self.Kheight+8), (0,255, 0), -1)
+                cv2.rectangle(self.MainIMG, (self.Klength+frame_offsetl,2),(2* self.Klength+4+frame_offsetl,self.Kheight+4), (0,255, 0), -1)
 
-                # updating video feed
+                # left frame image
+                self.MainIMG[rowmin:rowmax,colmin:colmax] = f
+                # right frame image
 
-                self.MainIMG[x1:y1,x2:y2] = f
+                self.MainIMG[rowmin:rowmax,colmax +frame_offsetl: 2*colmax -4 + frame_offsetl] = f
 
-                #self.MainIMG[x1+edge_off:y1 + edge_off,x2+edge_off:y2+ edge_off] =  frames[slide_frame ]
-
-                cv2.putText(self.MainIMG, frametxt, (10, 30), self.font, 1, (255, 255, 255), 0)
+                cv2.putText(self.MainIMG, frametxt, (2,self.Kheight+30 ), self.font, 1, (255, 255, 255), 0)
                 #cv2.imshow("video",f)
                 cv2.imshow(self.MainWindow, self.MainIMG)
 
+                self.MainIMG[self.Kheight+9:] = 0
+                #cv2.imshow(self.MainWindow,self.MainIMG)
 
-                self.MainIMG[:33] = 0
-                cv2.imshow(self.MainWindow,self.MainIMG)
+
+
+
                 #slide_frame = cv2.getTrackbarPos("Frame #", self.MainWindow)
                 if cv2.waitKey(1) & 0xff == ord('p'):
                     if cv2.waitKey(0) == 32:
@@ -116,17 +152,7 @@ class VideoRead():
                     else:
                         pass
                 frameCounter += 1
-            # reset topleft of frames for frame counter
-            #handle slider
-            # cv2.imshow(self.MainWindow, self.MainIMG)
-            slide_frame = cv2.getTrackbarPos("Frame #", self.MainWindow)
-            self.MainIMG[x1 + edge_off:y1 + edge_off, x2 + edge_off:y2 + edge_off] = frames[slide_frame]
-            cv2.imshow(self.MainWindow,self.MainIMG)
-            cv2.waitKey(1)
 
-
-
-            #print(slide_frame)
 
 
 
@@ -150,9 +176,9 @@ class VideoRead():
     #         #input("waiting:")
     #             #cv2.waitKey(100)
 
+frame_folder = "applebees/Frames/"
+V = VideoRead(frame_folder)
 
-V = VideoRead("video_15fps.mp4")
-V.load_frames()
 V.loop()
 # while 1:
 #     V.play_(1)
